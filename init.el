9f996e0a0d14283dd7710f345426876ae7fa237b
kienngynh <kien.ngynh@example.com>
2023-03-11 08:58:17 +0700

    ok

--- init.el
+++ init.el
@@ -39,6 +39,145 @@
 (setq insert-directory-program "gls" dired-use-ls-dired t)
 (setq dired-listing-switches "-al --group-directories-first")
 
+;; Early Init
+;; NOTE: Setting `font-lock-multiline' to 'undecided' org-mode to become unusable:
+;; Error during redisplay: (jit-lock-function 22138) signaled (args-out-of-range 0 16341)
+(setq-default font-lock-multiline nil)
+
+;;; Better defaults
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+(setq default-input-method nil)
+
+;; better simple ui
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(blink-cursor-mode 0)
+
+
+(setq visible-bell nil ;; set to non-nil to flash!
+      ring-bell-function 'ignore
+      large-file-warning-threshold (* 50 1024 1024) ;; change to 50 MiB
+      use-short-answers t ;; y or n istead of yes or no
+      confirm-kill-emacs 'y-or-n-p ;; confirm before quitting
+      ;; initial-scratch-message t
+      inhibit-startup-message t
+      frame-resize-pixelwise t
+      delete-by-moving-to-trash t)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;;(setq debug-on-error t)
+
+(setq epg-gpg-program "gpg2")
+
+(setq auth-sources '("~/.authinfo.gpg") ;; Defaults to GPG
+      auth-source-do-cache t
+      auth-source-cache-expiry 86400 ; All day, defaut is 2h (7200)
+      password-cache t
+      password-cache-expiry 86400)
+
+;;; Undo
+(setq undo-limit        10000000 ;; 1MB (default is 160kB)
+      undo-strong-limit 100000000 ;; 100MB (default is 240kB)
+      undo-outer-limit  1000000000) ;; 1GB (default is 24MB)
+
+;;; Editing
+;;(setq display-line-numbers-type 'relative
+;;      tab-always-indent nil
+;;      whitespace-action '(cleanup auto-cleanup))
+
+(setq-default truncate-lines nil
+              fill-column 140
+              indent-tabs-mode nil
+              display-line-numbers-width 3
+              tab-width 2)
+
+;;; Backups
+;; Disable backup and lockfiles
+(setq create-lockfiles nil
+      make-backup-files nil
+      version-control t ;; number each backup file
+      backup-by-copying t ;; copy instead of renaming current file
+      delete-old-versions t ;; clean up after itself
+      kept-old-versions 5
+      kept-new-versions 5
+      tramp-backup-directory-alist backup-directory-alist)
+
+;;; Auto-Saving, sessions...
+;; Enable auto-save (use `recover-file' or `recover-session' to recover)
+(setq auto-save-default t
+      auto-save-include-big-deletions t
+      auto-save-file-name-transforms
+      (list (list "\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'"
+                  ;; Prefix tramp autosaves to prevent conflicts with local ones
+                  (concat auto-save-list-file-prefix "tramp-\\2") t)
+            (list ".*" auto-save-list-file-prefix t)))
+
+(setq sentence-end-double-space nil)
+
+;;; Scrolling
+(setq hscroll-step 1
+      hscroll-margin 0
+      scroll-step 1
+      scroll-margin 0
+      scroll-conservatively 101
+      scroll-preserve-screen-position 'always
+      auto-window-vscroll nil
+      fast-but-imprecise-scrolling t)
+
+(setq-default scroll-up-aggressively 0.01
+              scroll-down-aggressively 0.01)
+
+(setq window-combination-resize t)
+
+(setq recentf-max-saved-items 100)
+
+;;; Enable global modes
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(add-hook 'text-mode-hook #'display-line-numbers-mode)
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+
+;; Scroll pixel by pixel
+(pixel-scroll-mode 1)
+
+;; Window layout undo/redo (`winner-undo' / `winner-redo')
+(winner-mode 1)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; Enable recentf-mode globally
+(recentf-mode 1)
+
+;; Save place in files
+(save-place-mode 1)
+
+;; Enable saving minibuffer history
+(savehist-mode 1)
+
+;; Show line and column numbers (cursor position) in mode-line
+(line-number-mode 1)
+
+;; Wrap long lines
+(global-visual-line-mode 1)
+
+;; Better handling for files with so long lines
+(global-so-long-mode 1)
+
+;; Global SubWord mode
+(global-subword-mode 1)
+
+;; Set Fullscreen
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Set Transparency
+(set-frame-parameter (selected-frame) 'alpha '(100 . 100))
+(add-to-list 'default-frame-alist '(alpha . (100 . 100)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
   ;; Change the user-emacs-directory to keep unwanted things out of ~/.emacs.d
   (setq user-emacs-directory (expand-file-name "~/.emacs.d/")
         url-history-file (expand-file-name "url/history" user-emacs-directory))
